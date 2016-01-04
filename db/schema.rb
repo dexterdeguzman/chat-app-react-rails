73d52540e8d93075b33b363e875d191025a22797
Dexter <dexter22252@gmail.com>
2016-01-04 14:56:02 +0800

    Adds migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208063516) do
+ActiveRecord::Schema.define(version: 20160104060450) do
+
+  create_table "channels", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
